17060669104a0ac89c9f922f5a43331a4a3d1b74
unknown <nile.sawakhande@gmail.com>
2020-01-14 20:24:50 +0530

    ...

--- server.js
+++ server.js
@@ -357,7 +357,7 @@ app.post('/honda/primary', (req, res) => {
 
 
 app.get('/honda/primary', (req, res) => {
-	console.log("honda primary get request" + JSON.stringify( req));
+	//console.log("honda primary get request" + JSON.stringify( req));
 	res.render('honda', { fail: false, otpSent: false, otpVerified: undefined, clientId:req.query.client_id, responseType: req.query.response_type, redirectURI: req.query.redirect_uri, scope: req.query.scope, State : req.query.state });
 });
 
