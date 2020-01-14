e48011b5397f06000611a7c608d69a23427510cd
unknown <nile.sawakhande@gmail.com>
2020-01-14 23:07:44 +0530

    ;;

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ app.post('/login',
 	passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
 	function (req, res) {
 		//console.log("value of req in  /login" + req)
-		console.log("value of req in  /login" + req.query)
+		
 		console.log("value of req in  /login" + req.query.next)
 		if (req.query.next) {
 
