e4574d2fbd3958aca89db55eeeeb885c4fd3d93e
Nilesh S <nile.sawakhande@gmail.com>
2020-03-05 19:33:13 +0530

    key changed

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
-const key = "Basic YwYzigQsp5v15WmH8SVxgsgQQpJN2Ut9ckapuHN7QEGbUxmLOYRaNRFmS7pkySEk+WEzlMAuWmryrh2UgA==";
+const key = "Basic mUsJIdb9L9FAvt5iiGHNep10ugRHzCZ5IFKfWFmoDxTcvcGkMse1o2zaSdtk1dmbXe9tRa4edu77";
 
 module.exports = key;
